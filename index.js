3c4a753a4fc428c9a8f70ddb6bceb9ce7e0dae18
Miguel Calixto <miguelcalixto@wolfsellers.com>
2022-10-18 21:03:09 -0500

    Conexion a la BD

---
+++ index.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_URI,
+                ()=>{
+                    console.log('Conexion a base de datos satisfactoria')
+                })
