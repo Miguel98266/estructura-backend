2c01e9711c4cc1ae87ca536717ecd7af9e80bf4b
Miguel Calixto <miguelcalixto@wolfsellers.com>
2022-10-19 20:47:10 -0500

    CRUD completo

--- index.js
+++ index.js
@@ -9,32 +9,35 @@ mongoose.connect(process.env.DB_URI, async () => {
   /**
    * CRUD basico con mongoose
    */
-  const user = await User.create({
-    address: "Direccion prueba",
-    birthday: new Date(),
-    contacts: ["Pedrito", "Jorge"],
-    email: "correo@prueba.com",
-    lastName: "Calixto",
-    name: "Miguel",
-    password: "123",
-    phone: "5561989427",
-    role: "Cliente",
-  });
-  console.log(user);
-  //   User.find({
-  //     name:"Miguel"
-  //   })
+//   const user = await User.create({
+//     address: "Direccion prueba",
+//     birthday: new Date(),
+//     contacts: ["Pedrito", "Jorge"],
+//     email: "correo@prueba.com",
+//     lastName: "Calixto",
+//     name: "Miguel",
+//     password: "123",
+//     phone: "5561989427",
+//     role: "Cliente",
+//   });
+//   console.log(user)
+//    const users=await User.find({
+//       name:"Miguel"
+//     })
+//     console.log(users);
 
-  //   User.updateMany({
-  //     name:"Miguel",
-  //     lastName:"Calixto"
-  //   },
-  //   {
-  //     role:"Admin"
-  //   }
-  //   )
-
-  //   User.deleteOne({
-  //     name:"Miguel"
-  //   })
+    // const promiseResult= await User.updateMany({
+    //   name:"Miguel",
+    //   lastName:"Calixto"
+    // },
+    // {
+    //   role:"Admin"
+    // }
+    // )
+    // console.log(promiseResult)
+    
+    // const result= await User.deleteOne({
+    //   name:"Miguel"
+    // })
+    // console.log(result)
 });
