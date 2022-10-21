64582c9122d73468c2842a06db2dc52db1c10d37
Miguel Calixto <miguelcalixto@wolfsellers.com>
2022-10-20 19:20:45 -0500

    Restaurant ejercicios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import User from "./api/models/User.js";
+import Book from "./api/models/Book.js";
 
 dotenv.config();
 
@@ -40,4 +41,38 @@ mongoose.connect(process.env.DB_URI, async () => {
     //   name:"Miguel"
     // })
     // console.log(result)
+
+
+    // CRUD BOOK
+    // Todo: Ejericio 1
+    // 1.-Crear modelo book
+    //? 2.- create de modelo book
+    
+    /*const createBook=await Book.create({
+        edition:1,
+        editorial:"Prueba",
+        isbn:"978-3-16-148410-0",
+        pageNumber:345,
+        title:"Backend Advanced"
+    })
+    console.log(createBook);
+    */
+    
+    
+    //? 3.- find de book
+    /* 
+    const findBook=await Book.find()
+    console.log(findBook);
+    */
+    //? 4.- Update Book by id
+    /* 
+    const updateBookId=await Book.findByIdAndUpdate(
+        "6350ac2ae6569b45351b6a7e",{
+        edition :2
+    })
+    console.log(updateBookId)
+    */
+    // 5.- Delete by id
+    /*const deleteBookid=await Book.findByIdAndDelete("6350ac2ae6569b45351b6a7e")
+    console.log(deleteBookid)*/
 });
